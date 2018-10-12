20506f29a343791b21db2d99bbfa9297757f85ed
Mykyta <arm454h@gmail.com>
2018-10-12 15:43:48 +0200

    Init.

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform car;
+    private Vector3 offset;
+    float rotationX;
+	// Use this for initializatio
+	void Start()
+    {
+        rotationX = transform.rotation.x;
+        offset = transform.position - car.position;
+    }
+    
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.eulerAngles = new Vector3(rotationX, car.eulerAngles.y,0);
+        transform.position = car.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerScripts/script.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+    public static readonly float MAX_EULER = 360;
+
+    int i = 0;
+
+    public void FixedUpdate()
+    {
+
+        //transform.
+        float motor = maxMotorTorque * Input.GetAxis("Vertical"); 
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        //float zRot = transform.rotation.z;
+        float zRot = transform.eulerAngles.z;
+        zRot = ClampEuler(zRot, -15, 15);
+
+        if (steering == 0.0)
+        {
+            //zRot = Mathf.MoveTowards(zRot, 0, 10 * Time.fixedDeltaTime);
+            zRot = StabilizeEuler(zRot, 10 * Time.fixedDeltaTime);
+        }
+        Debug.Log(zRot + " " + i++);
+        transform.eulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, zRot);
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+
+    //0 < zRot < 360
+    public static float StabilizeEuler(float val, float howFast)
+    {
+        if (val > 180.0)
+        {
+            return Mathf.MoveTowards(val, MAX_EULER, howFast);
+        }
+        else
+        {
+            return Mathf.MoveTowards(val, 0, howFast);
+        }
+    }
+
+    //0 < zRot < 360
+    public static float ClampEuler(float val, float min, float max)
+    {
+        if(val > 180)
+        {
+            return Mathf.Clamp(val, MAX_EULER - Mathf.Abs(min), MAX_EULER);
+        }
+        else
+        {
+            return Mathf.Clamp(val, 0, max);
+        }
+    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
