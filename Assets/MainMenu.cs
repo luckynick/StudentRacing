85e27c47b7787ca3aa7543ff9ec4162f1e22d5d9
adrianolsz <adeczko@gmail.com>
2018-11-30 13:41:31 +0100

    created second scene

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    int ID = 1;
+
+    public void ChangeID(int option)
+    {
+        this.ID = option+1;
+    }
+
+    public void PlayGame()
+    {
+        if(ID!=0)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + ID);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Gra wylaczona");
+        Application.Quit();
+    }
+
+}
